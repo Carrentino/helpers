4517f03f6c755464872e26e46035ed237b6e4e54
Михаил <mxoilslukin@gmail.com>
2025-03-04 16:06:30 +0500

    change redis client

---
+++ helpers/redis_client/client.py
@@ -0,0 +1,17 @@
+import redis.asyncio as redis
+from redis.asyncio import Redis
+
+
+class RedisClient:
+    def __init__(self, address: str = 'redis_client://localhost', db: int = 0) -> None:
+        self.address = address
+        self.db = db
+        self.redis = None
+
+    async def __aenter__(self) -> Redis:
+        self.redis = redis.from_url(self.address, db=self.db)
+        return self.redis
+
+    async def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        if self.redis:
+            await self.redis.close()
