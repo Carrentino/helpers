d7723a07426340bdefd05d99c60473c3384ae884
Михаил <mxoilslukin@gmail.com>
2025-03-07 17:27:27 +0500

    fix

--- helpers/utils.py
+++ helpers/utils.py
@@ -5,9 +5,9 @@ from helpers.models.response import PaginatedResponse
 
 async def get_paginated_response(data: list[Any], count: int, limit: int = 100, offset: int = 0) -> PaginatedResponse:
     return PaginatedResponse(
-        page=offset // limit + 1,
+        page=offset // limit if offset % limit == 0 else offset // limit + 1,
         size=limit,
         total=count,
-        total_pages=count // limit + 1,
+        total_pages=count // limit if count % limit == 0 else count // limit + 1,
         data=data,
     )
