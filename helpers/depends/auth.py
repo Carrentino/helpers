c5a844840b82d766161bbcae2aab8a260129b095
Михаил <mxoilslukin@gmail.com>
2025-03-31 20:48:31 +0500

    fixes

--- helpers/depends/auth.py
+++ helpers/depends/auth.py
@@ -11,16 +11,14 @@ from helpers.models.user import UserContext, UserStatus
 
 async def get_current_user(request: Request) -> UserContext:
     payload = request.scope.get('auth_token_payload')
-    print(payload)
 
     if not payload:
-        print(1)
         raise InvalidTokenError
 
     try:
         user_model = UserContext.model_validate(payload)
     except ValidationError as err:
-        print(2)
+        print(err)
         raise InvalidTokenError from err
 
     USER_CTX.set(user_model)
